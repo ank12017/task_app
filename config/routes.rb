6072464e3a88ac7d34c9774f8cfc86aee9f6bc45
anki12017 <ankitadangi11@gmail.com>
2017-10-31 17:35:37 +0530

    new commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+  root 'welcome#index'
   devise_for :teachers, controllers: { sessions: 'teachers/sessions', registrations: 'teachers/registrations'}
   resources :subjects
   resources :teachers
   resources :courses
-  get 'subjects/index'
-  root 'subjects#index'
+  resources :welcome
+ 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
